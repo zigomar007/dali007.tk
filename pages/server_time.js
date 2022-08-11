d6de8c315e06c2d17cd70eb0af7901dc69469da7
zigomar007 <103475660+zigomar007@users.noreply.github.com>
2022-08-11 06:15:02 +0100

    Add files via upload

---
+++ pages/server_time.js
@@ -0,0 +1,8 @@
+﻿$.ajax({
+        type: "POST",
+        data:"",
+        url: "ajax-get-server-time.php",
+        success: function(result){
+                $("#time_div").html(result);
+            }
+        });
